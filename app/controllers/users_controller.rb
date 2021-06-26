9bb22a95797630e96ffcb8c17965b2e61a52775b
EC2 Default User <ec2-user@ip-172-31-6-88.ap-northeast-1.compute.internal>
2021-06-26 10:32:15 +0000

    Finish add-user-attendances

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,14 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :set_one_month, only: :show
 
   def index
     @users = User.paginate(page: params[:page])
   end
 
   def show
+    @worked_sum = @attendances.where.not(started_at: nil).count
   end
 
   def new
@@ -46,10 +48,6 @@ class UsersController < ApplicationController
 
   def edit_basic_info
   end
-    
-  def basic_info_params
-    params.require(:user).permit(:department, :basic_time, :work_time)
-  end
 
   def update_basic_info
     if @user.update_attributes(basic_info_params)
@@ -60,35 +58,39 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
-  # beforeフィルター
-
-  # paramsハッシュからユーザーを取得します。
-  def set_user
-    @user = User.find(params[:id])
-  end
-
-  # ログイン済みのユーザーか確認します。
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "ログインしてください。"
-      redirect_to login_url
-    end
-  end
-
-  # アクセスしたユーザーが現在ログインしているユーザーか確認します。
-  def correct_user
-    redirect_to(root_url) unless current_user?(@user)
-  end
-
-  # システム管理権限所有かどうか判定します。
-  def admin_user
-    redirect_to root_url unless current_user.admin?
-  end
-    
   private
 
-  def user_params
-    params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
-  end
+    def user_params
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
+
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
+    end
+
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # ログイン済みのユーザーか確認します。
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # システム管理権限所有かどうか判定します。
+    def admin_user
+      redirect_to root_url unless current_user.admin?
+    end
 end
